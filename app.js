351eae27ba8a42787da64be7ec6af415a87a0bb6
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-09-13 14:52:57 -0400

    console logging

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert('hello');
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+const greeting = 'Hello';
+console.log(greeting);
+console.log([1,3,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+
+/*
+    This course is going to make use of the browsers console to test and run js code
+*/
