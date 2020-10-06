05e3175d0fc07333936d599fdfcf7d184573c154
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-05 21:34:05 -0400

    getting started with Object Oriented JS

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
-// Create Element
-const li = document.createElement('li');
+const person = {
+    name: 'Shiva',
+    age: 27
+}
 
-// Add Class
-li.className = 'collection-item';
-
-// Id Name
-li.id = 'new-item';
-
-// Add attributes
-li.setAttribute('title', 'New Item');
-
-// Create & Append text node
-li.appendChild(document.createTextNode('Hello World'));
-
-// Create new link element
-const link = document.createElement('a');
-
-// Add classes
-link.className = 'delete-item secondary-content';
-
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>'
-
-// Append link to li
-li.appendChild(link);
-
-// Append Li as child to UL
-document.querySelector('ul.collection').appendChild(li);
-
-console.log(li);
+console.log(person);
