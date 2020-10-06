d698092f45e4d4285ce3bdbb6a1a8d002aafeb61
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-05 21:45:21 -0400

    Created Person constructor with calculateAge function

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const person = {
-    name: 'Shiva',
-    age: 27
+// Person Constructor
+function Person(name, dob){
+    this.name = name;
+    // this.age = age;
+    this.birthday = new Date(dob);
+    this.calculateAge = function(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970); 
+    }
 }
 
-console.log(person);
+// const shiva = new Person('Shiva', 27);
+// const theo = new Person('Theo', 12);
+
+// console.log(shiva);
+// console.log(theo);
+
+const shiva = new Person('Shiva', '1992-12-20');
+console.log(shiva);
+
+console.log(shiva.calculateAge());
