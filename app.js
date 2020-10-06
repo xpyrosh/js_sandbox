da16a0ca9e1c3b276790db8a3578ecf195b78de6
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-06 19:36:59 -0400

    classes in ES6+

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-// Object.prototype
-// Person.prototype
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dob);
+    }
 
-// Person Constructor
-function Person(firstName, lastName){
-    this.firstName = firstName;
-    this.lastName = lastName; 
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
+
+    calculateAge(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    getsMarried(newLastName) {
+        this.lastName = newLastName;
+    }
+
+    static addNumbers(x,y){
+        return x+y;
+    }
 }
 
-// Prototype Method
-Person.prototype.greeting = function() {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-}
+const mary = new Person('Mary', 'Williams', '1950-12-12');
 
-const person1 = new Person('John', 'Doe');
+mary.getsMarried('Joe');
 
-// console.log(person1.greeting());
+console.log(mary);
 
-// Customer Constructor
-function Customer(firstName, lastName, phone, membership){
-    Person.call(this, firstName, lastName);
-    this.phone = phone;
-    this.membership = membership;
-}
-
-// Inherit Person Prototype to use methods
-Customer.prototype = Object.create(Person.prototype);
-
-// Make customer.prototype return Customer();
-Customer.prototype.constructor = Customer;
-
-// Create customer
-const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
-
-console.log(customer1);
-
-// Overwrite greeting method
-Customer.prototype.greeting = function(){
-    return `Hello there ${this.firstName} ${this.lastName} Welcome to our Company.`;
-}
-
-console.log(customer1.greeting());
+console.log(Person.addNumbers(1,2));
