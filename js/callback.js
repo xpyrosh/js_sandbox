3aed95ff47073817279b5d78874ebbe6cfc80cc9
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-08 22:24:45 -0400

    asynchronous not working

--- js/callback.js
+++ js/callback.js
@@ -1,11 +1,31 @@
-const posts = [
-    {title: 'Post One', body: 'This is post one'},
-    {title: 'Post Two', body: 'This is post two'}
-];
+// const posts = [
+//     {title: 'Post One', body: 'This is post one'},
+//     {title: 'Post Two', body: 'This is post two'}
+// ];
+
+// function createPost() {
+//     setTimeout(function() {
+//         posts.push(posts);
+//     }, 2000);
+// }
+
+// function getPosts(){
+//     setTimeout(function(){
+//         let output = '';
+//         posts.forEach(function(post){
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is Post Three'});
+
 
 function createPost() {
     setTimeout(function() {
         posts.push(posts);
+        callback();
     }, 2000);
 }
 
@@ -19,6 +39,4 @@ function getPosts(){
     }, 1000);
 }
 
-createPost({title: 'Post Three', body: 'This is Post Three'});
-
-getPosts();
+createPost({title: 'Post Three', body: 'This is Post Three'}, getPosts);
